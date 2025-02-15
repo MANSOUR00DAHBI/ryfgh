effd7b8922b4f63b4e3a3f21f7a849713c73322f
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-15 01:33:03 +0100

    set 2

---
+++ build.sh
@@ -0,0 +1,7 @@
+clear 
+#!/bin/sh
+set -xe 
+
+
+#Assemble with
+nasm -f bin boot.asm -o boot.bin.
--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 #include<stdio.h>
+// kernel.c
+void kmain(void) {
+    const char* str = "Hello from Mini Linux!";
+
+    // Use BIOS interrupt to print string
+    char* vidmem = (char*)0xB8000;  // Video memory address for color text mode
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        vidmem[i * 2] = str[i];
+        vidmem[i * 2 + 1] = 0x0F;  // Attribute byte for white text on black background
+    }
+
+    while (1); // Infinite loop to keep kernel running
+}
